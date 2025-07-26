b0a70012b9bde2ee326ad32fde11821cb7698fc8
mikeybkats <michaelbkats@gmail.com>
2025-07-26 07:04:53 -0700

    Adding answer for 2.69

--- src/2.69.c
+++ src/2.69.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+
+#include "library.h"
+
 /*
 Do rotating left shift
 
@@ -13,6 +17,29 @@ n  20 --> 0x67812345
 
 */
 
+int rotating_left_shift(int n, int x) {
+  // (x << n) | (x >> (32 - n)
+  int shift = n & 31;  // use & 31 to mask any bits above 31. works like modulo
+  int rotation = 32 - shift;
+  int leftShift = x << shift;
+  int rightShift = x >> rotation;
+  return leftShift | rightShift;
+}
+
 int main() {
+  int x = 0x12345678;
+
+  printf("%#.2x\n", x);
+  printf("%#.2x\n", rotating_left_shift(4, x));
+
+  printf("\n");
+
+  printf("%#.2x\n", x);
+  printf("%#.2x\n", rotating_left_shift(20, x));
+
+  printf("\n");
+
+  printf("%#.2x\n", x);
+  printf("%#.2x\n", rotating_left_shift(8, x));
   return 0;
 }
